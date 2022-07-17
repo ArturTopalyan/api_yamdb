3e33e88c19d16f26d6311479cd096cc3b232ee9c
Артур Топалян <topabro@yandex.ru>
2022-07-17 19:14:00 +0300

    static pls

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -110,11 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-# STATICFILES_DIRS необходимо закомментировать или удалить
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
